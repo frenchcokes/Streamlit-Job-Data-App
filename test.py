fa82961daada22e4e1c1526c26dbc6ec08fc1d87
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2024-02-19 18:37:06 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,18 +3,11 @@ import pandas as pd
 import os
 from streamlit_gsheets import GSheetsConnection
 
+# Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Job Applications",
-    ttl="0",
-    usecols=[3, 5],
-    nrows=200,
-)
+df = conn.read()
 
-f = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-st.table(f)
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
