6538d216be8814aa146c2b965171f06946260de8
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2024-02-19 17:57:01 -0700

    added gsheets connect

--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
+import os
+from streamlit_gsheets import GSheetsConnection
+
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
 
 f = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
 })
+
 st.table(f)
