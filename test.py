103ab850810fd9ee8a89693a9a4df1ba43e5624a
frenchcokes <144563579+frenchcokes@users.noreply.github.com>
2024-03-15 16:21:27 -0600

    fixed

--- test.py
+++ test.py
@@ -139,8 +139,8 @@ def createDeclinesGraph():
     declineCountForDaysFromStart = {}
     previous = 0
     for index, row in df.iterrows():
-        if(row["Date of Resp?"] != 0):
-            testDay = (pd.Timestamp(row["Date of Resp?"]) - pd.Timestamp(datetime(2023,9,20))).days
+        if(row["Date of Resp?"] != "N/A"):
+            testDay = (pd.Timestamp(row["Date of Resp?."]) - pd.Timestamp(datetime(2023,9,20))).days
             if(testDay in declineCountForDaysFromStart):
                 previous = previous + 1
                 declineCountForDaysFromStart[testDay] = declineCountForDaysFromStart[testDay] + 1
